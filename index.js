c3e5b66b0621a625a6a8e0a6438587bd08d8b61f
Kimberly West <kdenton993@gmail.com>
2022-05-02 16:09:39 -0600

    changes to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // glocbal require's
 const inquirer = require('inquirer');
-const fs - require('fs');
+const fs = require('fs');
 const generateMarkdown = require('./src/generateMarkdown.js')
 
 // array of questions
@@ -22,5 +22,21 @@ const questions = [
     // project description
 {
     type: 'input',
-    name: 'description'
+    name: 'description',
+    message: 'provide a description',
+    validate: descriptionInput => {
+        if (descriptionInput) {
+            return true;
+        } else {
+            console.log('you need a description!');
+            return false;
+        }
+    }
+},
+//installatiob instructions
+{
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install the project?',
+    validate: installationInput =>
 }]
