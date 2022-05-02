f12f3827905bc1ec06a5bb3999c3dfa29d05db94
Kimberly West <kdenton993@gmail.com>
2022-05-02 16:28:58 -0600

    added packages

--- index.js
+++ index.js
@@ -38,5 +38,51 @@ const questions = [
     type: 'input',
     name: 'installation',
     message: 'How do you install the project?',
-    validate: installationInput =>
+    validate: installationInput => {
+        if(installationInput) {
+            return true;
+        } else {
+            console.log('you need to provide installation info');
+            return false;
+        }
+    }
+},
+//usage info
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Gow do you ise this project?',
+    validate: usageInput => {
+        if (usageInput) {
+            return true;
+        } else {
+            console.log('you need to provide information on how to use the project');
+            return false;
+        }
+    }
+},
+//CONTRIBUTION GUIDLINES
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'how should people contribute to this prokect?',
+    validate: contributionInput => {
+        if (contributionInput) {
+            return true;
+        } else {
+            console.log('You need to provide information on how to contribute to the project!');
+            return false;
+        }
+    }
+},
+// test instructions
+{
+    type: 'input',
+    name: 'testing',
+    message: 'how do you test this project?',
+    validate: testingInput => {
+        if (testingInput) {
+            return true;
+        } else {console.log('you need to describe how to test this project!');}
+    }
 }]
